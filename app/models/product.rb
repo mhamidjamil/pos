5f0d6771b37de6ff78788778609db8ce5d3e78f8
mhamidjamil <55445517+mhamidjamil@users.noreply.github.com>
2025-07-18 23:59:09 +0500

    Add enum for unit_label in Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ApplicationRecord
   has_many :orders, through: :order_items
 
   validates :name, :quantity_in_stock, presence: true
+
+  enum unit_label: {
+    carton: "carton",
+    dozen: "dozen",
+    piece: "piece"
+  }, _suffix: true
 end
